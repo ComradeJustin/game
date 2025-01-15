1bf0b17d8322671e6e422e05bfd1719ad020bebe
Comradejustin <chinese2000ping@gmail.com>
2025-01-14 16:58:07 -0800

    small edit again
    Trying to figure out movement

--- scenes/Systems/Player.gd
+++ scenes/Systems/Player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 
 func input_reader():
 	var input_dir = Input.get_vector("move_left", "move_right","jump","move_down") 
-	var current_vel = input_dir * self.movement_spd
+
 	
 
 	
