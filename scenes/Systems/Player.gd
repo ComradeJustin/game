f408e68fd53673b7f9adf86fdc5fd4a115bb264d
Msp030909 <Macorrea0309@gmail.com>
2025-02-25 16:55:58 -0800

    we  be updating
    this will hopefully not blow up the earth

--- scenes/Systems/Player.gd
+++ scenes/Systems/Player.gd
@@ -1,10 +1,13 @@
 extends Entitydata
+var DashMax = 2
+var DashAmount = DashMax
+var direction = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.movement_spd = 300.0
-	# self.gravity = 9.81 * 1.0 * pow(1,2)
+	self.dash_vel = 100
 	pass # Replace with function body.
 
 
@@ -17,12 +20,35 @@ func _physics_process(delta: float) -> void:
 			velocity.y = -400.0 * 1.5
 			
 		var input_dir := Input.get_axis("move_left", "move_right") 
+		
 		if input_dir:
-			self.velocity.x = input_dir * movement_spd
+			self.velocity.x = input_dir * movement_spd * delta * dash_vel
 		else:
 			velocity.x = move_toward(velocity.x, 0, movement_spd)
+			
+		if input_dir < 0:
+			direction = false
+		elif input_dir > 0:
+			direction = true
+			
+		if Input.is_action_just_pressed("use_weapon"):
+			if direction == false:
+				print("left")
+				if $LWeaponHitbox.is_colliding():
+					if $LWeaponHitbox.get_collider().class == "entity":
+						print("who out there collidin")
+			else:
+				print("right")
+				
 		
-	
+		if Input.is_action_just_pressed("Dash") and !is_on_floor():
+			if DashAmount > 0:
+				self.velocity.x = input_dir * movement_spd / delta
+				DashAmount -= 1;
+				
+		if is_on_floor():
+			DashAmount = DashMax
+
 		move_and_slide()
 
 
