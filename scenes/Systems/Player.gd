1b7d615bce985f9aab9f924eb95d558c6e8e9a84
Msp030909 <Macorrea0309@gmail.com>
2025-02-25 16:10:05 -0800

    Game now keeps track of player direction
    press either e or mouse click to get the direction of the player
    
    Planning to use this for the weapon system

--- scenes/Systems/Player.gd
+++ scenes/Systems/Player.gd
@@ -1,6 +1,8 @@
 extends Entitydata
 var DashMax = 2
 var DashAmount = DashMax
+var direction = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,10 +20,22 @@ func _physics_process(delta: float) -> void:
 			velocity.y = -400.0 * 1.5
 			
 		var input_dir := Input.get_axis("move_left", "move_right") 
+		
 		if input_dir:
 			self.velocity.x = input_dir * movement_spd * delta * dash_vel
 		else:
 			velocity.x = move_toward(velocity.x, 0, movement_spd)
+			
+		if input_dir < 0:
+			direction = false
+		elif input_dir > 0:
+			direction = true
+			
+		if Input.is_action_just_pressed("use_weapon"):
+			if direction == false:
+				print("left")
+			else:
+				print("right")
 		
 		if Input.is_action_just_pressed("Dash") and !is_on_floor():
 			if DashAmount > 0:
