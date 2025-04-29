bdccd6834b0c67afab3626eaf3a558f048348978
Msp030909 <Macorrea0309@gmail.com>
2025-04-29 16:25:00 -0700

    no one hears a word they say
    has the memory gone are you feeling numb
    (pause screen)

--- Assets/Systems/Game_Systems/global_script.gd
+++ Assets/Systems/Game_Systems/global_script.gd
@@ -1,6 +1,4 @@
 extends Node2D
-var pause = load("res://Assets/debug_objects/test_scenes/pause screen.tscn")
-var instPause =  pause.instantiate()
 
 var pausestate:bool
 func _ready():
@@ -14,9 +12,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Pause"):
 		if pausestate == true:
 			pausestate = false
+			$Ui/Pause.show()
 			get_tree().paused = true
 			print("pause")
 		elif pausestate == false:
+			$Ui/Pause.hide()
 			get_tree().paused = false
 			pausestate = true
 			print("unpause")
